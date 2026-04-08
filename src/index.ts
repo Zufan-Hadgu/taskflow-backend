4f1137e03112f1a96d984453fc7d93fa54844321
Zufan Gebrehiwot <zufanhadgu1@gmail.com>
2026-04-08 12:46:15 +0300

    feat: implement async file reader and http server

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
 import {promises as fs} from "node:fs";
+import http from "node:http"
+import {join }from "node:path"
+
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+
+// Recreate __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 async function readUsers() {
    try{
-      const filePath = "./src/data/mock-users.json"
+      const filePath = join(__dirname, "..", "src/data", "mock-users.json");
       const data = await fs.readFile(filePath,"utf-8")
       console.log(data)
    }catch(error){
@@ -11,3 +21,21 @@ async function readUsers() {
 }
 
 readUsers()
+const server = http.createServer(async (req,res) =>{
+   console.log("request received")
+   if (req.url === "/projects" && req.method =="GET"){
+
+      const filePath = join(__dirname, "..", "src/data", "mock-projects.json");
+      const data = await fs.readFile(filePath, "utf-8");
+      res.setHeader("Content-Type","application/json")
+
+      res.end(data)
+   }else {
+      res.end("not found")
+   }
+
+})
+
+server.listen(3000,() =>{
+   console.log("server running on porst 3000")
+})
