ebde50c89292810a1b2881ec2368bf063304b133
Zufan Gebrehiwot <zufanhadgu1@gmail.com>
2026-04-08 13:16:30 +0300

    refactor: modularize file reading and add status code handling

--- src/index.ts
+++ src/index.ts
@@ -4,35 +4,57 @@ import {join }from "node:path"
 
 import { fileURLToPath } from 'node:url';
 import { dirname } from 'node:path';
+import { stringify } from "node:querystring";
 
 // Recreate __dirname
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+async function getJsonData(fileName:string){
+   try{
+      const filePath = join(__dirname, "..", "src/data", fileName);
+      const data = await fs.readFile(filePath,"utf-8")
+      return JSON.parse(data);
+
+   }catch(error){
+      throw new Error("FileSystem Error")
+   }
+}
 async function readUsers() {
    try{
-      const filePath = join(__dirname, "..", "src/data", "mock-users.json");
-      const data = await fs.readFile(filePath,"utf-8")
-      console.log(data)
+      const user = await getJsonData("mock-user.json")
+      console.log(user)
    }catch(error){
       console.log("problem happend",error)
    }
   
 }
 
-readUsers()
+
 const server = http.createServer(async (req,res) =>{
-   console.log("request received")
-   if (req.url === "/projects" && req.method =="GET"){
+   res.setHeader("Content-Type","application/json")
+   try{
+      if (req.url === "/projects" && req.method =="GET"){
+      const projects = await getJsonData("mock-projects.json")
+      res.writeHead(200)
+      res.end(JSON.stringify(projects))
 
-      const filePath = join(__dirname, "..", "src/data", "mock-projects.json");
-      const data = await fs.readFile(filePath, "utf-8");
-      res.setHeader("Content-Type","application/json")
-
-      res.end(data)
-   }else {
-      res.end("not found")
    }
+   else {
+      res.writeHead(404)
+      res.end(JSON.stringify({
+         error:"Not found",
+      }))
+   }
+
+
+   }catch(error) {
+      res.writeHead(500);
+      res.end(JSON.stringify({
+         error:"Internal server error"
+      }))
+   }
+
 
 })
 
